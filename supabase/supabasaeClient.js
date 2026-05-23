be548413c85d6fd4258c698c22339603f1807d00
imenapr <imenapro14@gmail.com>
2026-05-23 22:08:41 +0400

    rem files

--- supabase/supabasaeClient.js
+++
@@ -1,8 +0,0 @@
-import {createClient} from '@supabase/supabase-js'
-
-const supabaseUrl = ProcessingInstruction.env.SUPABASE_URL
-const supabaseKey = ProcessingInstruction.env.SUPABASE_KEY
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
